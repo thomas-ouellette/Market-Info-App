e88861f26d46b705322ab850e96e808bfd638ea9
Thomas Ouellette <thomas.ouellette@plusgrade.com>
2024-10-02 15:50:33 -0400

    Removed hard-coded secret.

--- MarketInfoApp/MarketInfoApp/settings.py
+++ MarketInfoApp/MarketInfoApp/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-q&@dt-=rn9a$l)96c%fg0b2-72g)padvi=g(@_9nw@(l_uf$4-'
+import os
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
